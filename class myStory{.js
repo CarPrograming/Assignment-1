3d796163296e32bedb5152d60d57bdf5aa18eb04
CarPrograming <carlosenm.03@gmail.com>
2024-10-08 13:02:33 -0400

    Initial proyect

---
+++ class myStory{.js
@@ -0,0 +1,19 @@
+class myStory{
+   constructor(Subject,action,adjectives,objects,places){
+      this.subject=subject;
+      this.action=action;
+      this.adjectives=adjective;
+      this.objects=objects;
+      this.places=places;
+   }
+
+
+   bottonSubject.onclick = function(){
+      let counter = 0;
+  }
+
+
+
+
+
+}
