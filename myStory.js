23e47f507eef20f41cebaec1e80d8240cd075889
CarPrograming <carlosenm.03@gmail.com>
2024-10-08 13:14:32 -0400

    creating the variable for bottons and relating then with html

---
+++ myStory.js
@@ -0,0 +1,25 @@
+class myStory{
+   constructor(Subject,action,adjectives,objects,places){
+      this.subject=subject;
+      this.action=action;
+      this.adjectives=adjective;
+      this.objects=objects;
+      this.places=places;
+   }
+
+  
+}
+
+const Btn1 = document.querySelector('#Btn1');
+const Btn2 = document.querySelector('#Btn2');
+const Btn3 = document.querySelector('#Btn3');
+const Btn4 = document.querySelector('#Btn4');
+const Btn5 = document.querySelector('#Btn5');
+
+
+
+
+let forBotton1 = ["manzana", "banana", "naranja"];
+Btn1.onclick = function(){
+   let counter = 0;
+}
